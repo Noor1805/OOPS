00de790adef8df391148c626e9d5429b8f1da6f3
Noor1805 <kashafnoor346@gmail.com>
2025-12-18 18:20:29 +0530

    cmplted other 10 file

---
+++ Module1/program11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 5;
+
+    printf("Add = %d\n", a + b);
+    printf("Sub = %d\n", a - b);
+    printf("Mul = %d\n", a * b);
+    printf("Div = %d\n", a / b);
+
+    return 0;
+}
---
+++ Module1/program13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1, b = 0;
+
+    printf("%d\n", a && b);
+    printf("%d\n", a || b);
+    printf("%d\n", !a);
+
+    return 0;
+}
