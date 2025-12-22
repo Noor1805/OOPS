635eb9a1d676e84ad91f336e3cfb013dbedb9b9e
Noor1805 <kashafnoor346@gmail.com>
2025-12-22 18:09:11 +0530

    5 done

---
+++ Module3/program89.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void show() { cout << "A"; }
+};
+
+class B {
+public:
+    void show() { cout << "B"; }
+};
+
+class C : public A, public B {};
+
+int main() {
+    C obj;
+    // obj.show();  // ambiguous
+    return 0;
+}
